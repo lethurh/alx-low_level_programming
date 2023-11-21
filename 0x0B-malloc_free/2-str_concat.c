9401983839aaf5f287f897e7b92412964ad4993f
lethurh <gugulethupraxedis@gmai.com>
2023-11-20 19:13:51 -0800

    Write a function that splits a string into words.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,35 +2,42 @@
 #include <stdlib.h>
 
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *str;
-	int i, r;
+	char *strout;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = r = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[r] != '\0')
-		r++;
-	str = malloc(sizeof(char) * (i + r + 1));
 
-	if (str == NULL)
-		return (NULL);
-	i = r = 0;
-	while (s1[i] != '\0')
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
 	{
-		str[i] = s1[i];
-		i++, r++;
+		free(strout);
+		return (NULL);
 	}
-	str[i] = '\0';
-	return (str);
+
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
 }
